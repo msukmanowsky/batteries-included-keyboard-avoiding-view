96c7fa08ca4a64d417ab054e6e149843dd6e6a9e
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2019-04-26 23:47:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+A KeyboardAvoidingView that works out of the box. Inspired by [Nick Yang](https://medium.com/@nickyang0501/keyboardavoidingview-not-working-properly-c413c0a200d4)
+
+- Uses a `TouchableWithoutFeedback` wrapper to dismiss the keyboard
+- Uses `behavior="padding"` as a consistent default
+- Actually works!
+
---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView as RNKeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  View,
+  ViewPropTypes,
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+
+export default class KeyboardAvoidingView extends React.Component {
+
+  static propTypes = {
+    KeyboardAvoidingViewProps: RNKeyboardAvoidingView.propTypes,
+    keyboardAvoidingViewStyle: ViewPropTypes.style,
+    innerViewStyle: ViewPropTypes.style,
+  }
+
+  static defaultProps = {
+    KeyboardAvoidingViewProps: {},
+    keyboardAvoidingViewStyle: {},
+    innerViewStyle: {}
+  }
+
+  render() {
+    const {
+      children,
+      innerViewStyle,
+      KeyboardAvoidingViewProps,
+      keyboardAvoidingViewStyle,
+    } = this.props;
+
+    return (
+      <KeyboardAvoidingView
+        behavior="padding"
+        style={{
+          flex: 1,
+          ...keyboardAvoidingViewStyle,
+        }}
+        {...KeyboardAvoidingViewProps}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View
+            style={{
+              justifyContent: 'flex-end',
+              ...innerViewStyle,
+            }}
+          >
+            { children }
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    );
+  }
+}
